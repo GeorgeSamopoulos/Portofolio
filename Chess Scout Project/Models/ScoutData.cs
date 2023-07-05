119e2eab3cd093edc74da7d0520a1a807a25c397
GeorgeSamopoulos <134342388+GeorgeSamopoulos@users.noreply.github.com>
2023-07-05 15:05:27 +0300

    Add files via upload

---
+++ Chess Scout Project/Models/ScoutData.cs
@@ -0,0 +1,31 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace ChessScout.Models;
+
+public partial class ScoutData
+{
+    [JsonProperty("PlayerName")]
+    public string? PlayerName { get; set; }
+
+    [JsonProperty("UserName")]
+    public string? UserName { get; set; }
+
+    [JsonProperty("Elo")]
+    public int? Elo { get; set; }
+
+    [JsonProperty("preferted_openings_data")]
+    public string? preferted_openings_data { get; set; }
+
+    [JsonProperty("games")]
+    public string? games { get; set; }
+
+    [JsonProperty("opening_tree_data")]
+    public string? opening_tree_data { get; set; }
+
+    public int? Pid { get; set; }
+
+    public int? Uid { get; set; }   
+}
